1cdd6aab33da163b0e73cfdf673785770822733a
Ralf Kilian <github@urbanware.org>
2017-11-06 14:25:43 +0100

    Add files via upload

--- core/main.py
+++ core/main.py
@@ -44,12 +44,103 @@ class ByteWeiser(object):
     def __init__(self):
         return
 
+    def compare_and_replace(self, file_input, file_output, buffer_size=4096,
+                            simulate=False, verbose=True, progress=True):
+        """
+            Main function to compare and replace the bytes (or the blocks).
+        """
+        file_input = os.path.abspath(file_input)
+        file_output = os.path.abspath(file_output)
+
+        pv.path(file_input, "input", True, True)
+        pv.path(file_output, "output", True, True)
+        pv.intvalue(buffer_size, "buffer size", True, False, False)
+
+        if file_input == file_output:
+            raise Exception("The path of the input and output file must not " \
+                            "be the same.")
+
+        self.__file_input = file_input
+        self.__file_input_size = common.get_file_size(file_input)
+        self.__file_output = file_output
+        self.__file_output_size = common.get_file_size(file_output)
+
+        self.__buffer_size = int(buffer_size)
+        self.__simulate = simulate
+        self.__verbose = verbose
+
+        if self.__file_input_size > self.__file_output_size:
+            self.__padding = len(str(self.__file_input_size))
+        else:
+            self.__padding = len(str(self.__file_output_size))
+
+        self.__byte_blocks = int(self.__file_input_size / self.__buffer_size)
+        self.__byte_remainder = self.__file_input_size % self.__buffer_size
+
+        self.print_input_info(progress)
+
+        if verbose:
+            self.__timestamp = dt.now()
+
+        fh_input = open(file_input, "rb")
+        if simulate:
+            fh_output = open(file_output, "rb")
+        else:
+            fh_output = open(file_output, "r+b")
+
+        data_input = bytearray(b"")
+        data_output = bytearray(b"")
+
+        pos = 0
+        for block in range(self.__byte_blocks):
+            data_input = bytearray(b"")
+            data_output = bytearray(b"")
+            if progress:
+                self.print_status(pos, block)
+
+            data_input.extend(fh_input.read(self.__buffer_size))
+            data_output.extend(fh_output.read(self.__buffer_size))
+            if not data_input == data_output:
+                fh_output.seek(pos)
+                if not simulate:
+                    fh_output.write(data_input)
+                else:
+                    fh_output.seek(pos + self.__buffer_size)
+                self.__replaced_blocks += 1
+                self.__replaced_bytes += self.__buffer_size
+            else:
+                self.__untainted_blocks += 1
+                self.__untainted_bytes += self.__buffer_size
+            pos += self.__buffer_size
+
+        if self.__byte_remainder > 0:
+            data_input = bytearray(b"")
+            data_output = bytearray(b"")
+            data_input.extend(fh_input.read(self.__byte_remainder))
+            data_output.extend(fh_output.read(self.__byte_remainder))
+            if not data_input == data_output:
+                fh_output.seek(pos)
+                if not simulate:
+                    fh_output.write(data_input)
+                self.__replaced_blocks += 1
+                self.__replaced_bytes += self.__byte_remainder
+            else:
+                self.__untainted_blocks += 1
+                self.__untainted_bytes += self.__byte_remainder
+
+        if not simulate:
+            fh_output.truncate()
+        fh_output.close()
+        fh_input.close()
+
+        self.print_output_info(progress)
+
     def format_string(self, string, number):
         if not number == 1:
             string += "s"
         return string
 
-    def print_input_info(self):
+    def print_input_info(self, progress):
         """
             Print information about the input data.
         """
@@ -87,8 +178,11 @@ class ByteWeiser(object):
             else:
                 print("--[ ByteWeiser process ]".ljust(78, "-"))
             print()
+            if not progress:
+                sys.stdout.write("Processing files. Please wait.\r")
+                sys.stdout.flush()
 
-    def print_output_info(self):
+    def print_output_info(self, progress):
         """
             Print results after the main process has been finished.
         """
@@ -98,9 +192,13 @@ class ByteWeiser(object):
                 p = 100
             percent = ("%.4f" % p)
 
-            print("Total progress:     %s %% (finished, elapsed time: %s)" % \
-                (str("100").rjust(self.__padding, " "),
-                 (dt.now() - self.__timestamp)))
+            if progress:
+                print("Total progress:     %s %% (finished, elapsed time: %s)" \
+                    % (str("100").rjust(self.__padding, " "),
+                      (dt.now() - self.__timestamp)))
+            else:
+                print("Finished. Elapsed time: %s" \
+                    % (dt.now() - self.__timestamp))
             print()
             print("Replaced blocks:    %s %s" % \
                 (str(self.__replaced_blocks).rjust(self.__padding, " "),
@@ -127,7 +225,7 @@ class ByteWeiser(object):
         if self.__verbose:
             try:
                 char = block % len(self.__chars_busy)
-                if (pos % int(self.__byte_blocks / 1000)) == 0:
+                if (pos % int(self.__byte_blocks / 100000)) == 0:
                     percent = float(block) / self.__byte_blocks * 100
                     sys.stdout.write( \
                         "Total progress:     %s %s" % \
@@ -137,96 +235,6 @@ class ByteWeiser(object):
                     sys.stdout.flush()
             except:
                 pass
-
-    def compare_and_replace(self, file_input, file_output, buffer_size=4096,
-                            simulate=False, verbose=True):
-        """
-            Main function to compare and replace the bytes (or the blocks).
-        """
-        file_input = os.path.abspath(file_input)
-        file_output = os.path.abspath(file_output)
-
-        pv.path(file_input, "input", True, True)
-        pv.path(file_output, "output", True, True)
-        pv.intvalue(buffer_size, "buffer size", True, False, False)
-
-        if file_input == file_output:
-            raise Exception("The path of the input and output file must not " \
-                            "be the same.")
-
-        self.__file_input = file_input
-        self.__file_input_size = common.get_file_size(file_input)
-        self.__file_output = file_output
-        self.__file_output_size = common.get_file_size(file_output)
-
-        self.__buffer_size = int(buffer_size)
-        self.__simulate = simulate
-        self.__verbose = verbose
-
-        if self.__file_input_size > self.__file_output_size:
-            self.__padding = len(str(self.__file_input_size))
-        else:
-            self.__padding = len(str(self.__file_output_size))
-
-        self.__byte_blocks = int(self.__file_input_size / self.__buffer_size)
-        self.__byte_remainder = self.__file_input_size % self.__buffer_size
-
-        self.print_input_info()
-
-        if verbose:
-            self.__timestamp = dt.now()
-
-        fh_input = open(file_input, "rb")
-        if simulate:
-            fh_output = open(file_output, "rb")
-        else:
-            fh_output = open(file_output, "r+b")
-
-        data_input = bytearray(b"")
-        data_output = bytearray(b"")
-
-        pos = 0
-        for block in range(self.__byte_blocks):
-            data_input = bytearray(b"")
-            data_output = bytearray(b"")
-            self.print_status(pos, block)
-
-            data_input.extend(fh_input.read(self.__buffer_size))
-            data_output.extend(fh_output.read(self.__buffer_size))
-            if not data_input == data_output:
-                fh_output.seek(pos)
-                if not simulate:
-                    fh_output.write(data_input)
-                else:
-                    fh_output.seek(pos + self.__buffer_size)
-                self.__replaced_blocks += 1
-                self.__replaced_bytes += self.__buffer_size
-            else:
-                self.__untainted_blocks += 1
-                self.__untainted_bytes += self.__buffer_size
-            pos += self.__buffer_size
-
-        if self.__byte_remainder > 0:
-            data_input = bytearray(b"")
-            data_output = bytearray(b"")
-            data_input.extend(fh_input.read(self.__byte_remainder))
-            data_output.extend(fh_output.read(self.__byte_remainder))
-            if not data_input == data_output:
-                fh_output.seek(pos)
-                if not simulate:
-                    fh_output.write(data_input)
-                self.__replaced_blocks += 1
-                self.__replaced_bytes += self.__byte_remainder
-            else:
-                self.__untainted_blocks += 1
-                self.__untainted_bytes += self.__byte_remainder
-
-        if not simulate:
-            fh_output.truncate()
-        fh_output.close()
-        fh_input.close()
-
-        self.print_output_info()
 
 # EOF
 
