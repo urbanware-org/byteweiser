a84b4cad39968b241878cb8b2e87a3ae367804a8
urbanware-org <github@urbanware.org>
2019-06-12 20:51:54 +0200

    Update PaVal core module

--- core/paval.py
+++ core/paval.py
@@ -3,58 +3,61 @@
 
 # ============================================================================
 # PaVal - Parameter validation module
-# Copyright (C) 2016 by Ralf Kilian
+# Copyright (C) 2018 by Ralf Kilian
 # Distributed under the MIT License (https://opensource.org/licenses/MIT)
 #
-# Website: http://www.urbanware.org
 # GitHub: https://github.com/urbanware-org/paval
+# GitLab: https://gitlab.com/urbanware-org/paval
 # ============================================================================
 
-__version__ = "1.2.5"
+__version__ = "1.2.7"
 
 import filecmp
 import os
 
-def compfile(path, name="", list_files=[]):
+
+def compfile(input_path, name="", list_files=None):
     """
         Compare files to avoid that the same file is given multiple times or
         in different ways (e. g. different name but same content).
     """
-    __string(path, "%s path" % name, True)
+    __string(input_path, "%s path" % name, True)
 
-    if list_files == None:
+    if list_files is None:
         list_files = []
-    elif len(list_files) == 0:
+    elif not list_files:
         __ex("File list is empty (no files to compare with).", True)
     else:
         for item in list_files:
-            if not type(item) == list:
+            if not isinstance(item, list):
                 __ex("Every list item must be a sub-list.", True)
             if not len(item) == 2:
                 __ex("Every sub-list must contain two items.", True)
 
-    path = os.path.abspath(path)
+    input_path = os.path.abspath(input_path)
     for item in list_files:
         path_compare = os.path.abspath(str(item[0]))
         name_compare = str(item[1])
-        if path == path_compare:
+        if input_path == path_compare:
             __ex("The %s and the %s file path must not be identical." %
                  (name, name_compare), False)
-        if os.path.exists(path) and os.path.exists(path_compare):
-            if filecmp.cmp(path, path_compare, 0):
+        if os.path.exists(input_path) and os.path.exists(path_compare):
+            if filecmp.cmp(input_path, path_compare, 0):
                 __ex("The %s and %s file content must not be identical." %
                      (name, name_compare), False)
 
-def compstr(string, name="", list_strings=[]):
+
+def compstr(input_string, name="", list_strings=None):
     """
         Compare a string with a list of strings and check if it is an item of
         that list.
     """
-    __string(string, name, False)
-    if len(list_strings) == 0:
+    __string(input_string, name, False)
+    if not list_strings:
         __ex("No %s strings to compare with." % name, True)
-    if not string in list_strings:
-        __ex("The %s '%s' does not exist." % (name, string), False)
+    if input_string not in list_strings:
+        __ex("The %s '%s' does not exist." % (name, input_string), False)
+
 
 def get_version():
     """
@@ -62,39 +65,41 @@ def get_version():
     """
     return __version__
 
+
 def intrange(value, name="", value_min=None, value_max=None, zero=False):
     """
         Validate an integer range.
     """
     value = __integer(value, "%s value" % name, False)
-    if not value_min == None:
+    if value_min is not None:
         value_min = __integer(value_min, "minimal %s value" % name, True)
         intvalue(value_min, name, True, True, True)
-    if not value_max == None:
+    if value_max is not None:
         value_max = __integer(value_max, "maximal %s value" % name, True)
         intvalue(value_max, name, True, True, True)
     if not zero:
         if value == 0:
             __ex("The %s value must not be zero." % name, False)
-    if (not value_min == None) and (not value_max == None):
+    if (value_min is not None) and (value_max is not None):
         if value_min > value_max:
             __ex("The maximal %s value must be greater than the minimal "
                  "value." % name, False)
-        if (value_min == value_max) and (not value == value_min):
-            __ex("The %s value can only be %s (depending on further range " \
+        if (value_min == value_max) and (value != value_min):
+            __ex("The %s value can only be %s (depending on further range "
                  "further range arguments)." % (name, value_min), False)
         if (value < value_min) or (value > value_max):
-            __ex("The %s value must be between %s and %s (depending on " \
+            __ex("The %s value must be between %s and %s (depending on "
                  "further range arguments)." % (name, value_min, value_max),
                  False)
-    elif not value_min == None:
+    elif value_min is not None:
         if value < value_min:
             __ex("The %s value must not be less than %s." % (name, value_min),
                  False)
-    elif not value_max == None:
+    elif value_max is not None:
         if value > value_max:
             __ex("The %s value must not be greater than %s." %
                  (name, value_max), False)
+
 
 def intvalue(value, name="", positive=True, zero=False, negative=False):
     """
@@ -111,70 +116,71 @@ def intvalue(value, name="", positive=True, zero=False, negative=False):
         if value < 0:
             __ex("The %s value must not be negative." % name, False)
 
-def path(path, name="", is_file=False, exists=False, system_dirs=False):
+
+def path(input_path, name="", is_file=False, exists=False):
     """
         Validate a path of a file or directory.
     """
-    string(path, "%s path" % name, False, None)
-    path = os.path.abspath(path)
-
-    if not system_dirs:
-        pass
+    string(input_path, "%s path" % name, False, None)
+    input_path = os.path.abspath(input_path)
 
     if is_file:
         path_type = "file"
     else:
         path_type = "directory"
     if exists:
-        if not os.path.exists(path):
+        if not os.path.exists(input_path):
             __ex("The given %s %s does not exist." % (name, path_type), False)
-        if (is_file and not os.path.isfile(path)) or \
-           (not is_file and not os.path.isdir(path)):
+        if (is_file and not os.path.isfile(input_path)) or \
+           (not is_file and not os.path.isdir(input_path)):
             __ex("The given %s %s path is not a %s." % (name, path_type,
                                                         path_type), False)
     else:
-        if os.path.exists(path):
+        if os.path.exists(input_path):
             __ex("The given %s %s path already exists." % (name, path_type),
                  False)
 
-def string(string, name="", wildcards=False, invalid_chars=None):
+
+def string(input_string, name="", wildcards=False, invalid_chars=None):
     """
         Validate a string.
     """
-    __string(string, name, False)
-    if invalid_chars == None:
+    __string(input_string, name, False)
+    if invalid_chars is None:
         invalid_chars = ""
     if not wildcards:
-        if ("*" in string) or ("?" in string):
+        if ("*" in input_string) or ("?" in input_string):
             __ex("The %s must not contain wildcards." % name, False)
-    if len(invalid_chars) > 0:
+    if invalid_chars:
         for char in invalid_chars:
-            if char in string:
+            if char in input_string:
                 # Use single quotes by default or double quotes in case the
                 # single quotes are the invalid character
                 quotes = "'"
                 if char == quotes:
                     quotes = "\""
 
-                __ex("The %s contains at least one invalid character " \
+                __ex("The %s contains at least one invalid character "
                      "(%s%s%s)." % (name, quotes, char, quotes), False)
 
-def __ex(string, internal=False):
+
+def __ex(exception_string, internal=False):
     """
         Internal method to raise an exception.
     """
-    string = str(string).strip()
-    while (" " * 2) in string:
-        string = string.replace((" " * 2), " ")
+    ex = str(exception_string).strip()
+    while " " * 2 in ex:
+        ex = ex.replace((" " * 2), " ")
     if internal:
-        string = "PaVal: " + string
-    raise Exception(string)
+        ex = "PaVal: " + ex
+    raise Exception(ex)
+
 
 def __integer(value, name="", internal=False):
     """
         Internal method for basic integer validation.
     """
-    if value == None:
+    if value is None:
         __ex("The %s is missing." % name, internal)
     if value == "":
         __ex("The %s must not be empty." % name, internal)
@@ -184,14 +190,14 @@ def __integer(value, name="", internal=False):
         __ex("The %s must be an integer." % name, internal)
     return int(value)
 
-def __string(string, name="", internal=False):
+
+def __string(input_string, name="", internal=False):
     """
         Internal method for basic string validation.
     """
-    if string == None:
+    if input_string is None:
         __ex("The %s is missing." % name, internal)
-    if string == "":
+    if input_string == "":
         __ex("The %s must not be empty." % name, internal)
 
 # EOF
-
