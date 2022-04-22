6ca43c1f7906ca3b5e37ebbd39f5e87efff5b190
urbanware-org <github@urbanware.org>
2022-04-22 23:02:08 +0200

    Replace current PaVal core module with new version

--- core/paval.py
+++ core/paval.py
@@ -1,50 +1,53 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# ============================================================================
+#
 # PaVal - Parameter validation module
-# Copyright (C) 2018 by Ralf Kilian
+# Copyright (C) 2022 by Ralf Kilian
 # Distributed under the MIT License (https://opensource.org/licenses/MIT)
 #
 # GitHub: https://github.com/urbanware-org/paval
 # GitLab: https://gitlab.com/urbanware-org/paval
-# ============================================================================
+#
 
-__version__ = "1.2.7"
+__version__ = "1.3.0"
 
 import filecmp
 import os
 
 
-def compfile(input_path, name="", list_files=None):
+def compfile(file_path, name="", list_files=None):
     """
         Compare files to avoid that the same file is given multiple times or
-        in different ways (e. g. different name but same content).
+        in different ways (e.g. different name but same content).
     """
-    __string(input_path, "%s path" % name, True)
+    __string(file_path, "%s path" % name, True)
 
     if list_files is None:
         list_files = []
     elif not list_files:
-        __ex("File list is empty (no files to compare with).", True)
+        __ex("File list is empty (no files to compare with).", True,
+             ValueError)
     else:
         for item in list_files:
             if not isinstance(item, list):
-                __ex("Every list item must be a sub-list.", True)
+                __ex("Every list item must be a sub-list.", True,
+                     ValueError)
             if not len(item) == 2:
-                __ex("Every sub-list must contain two items.", True)
+                __ex("Every sub-list must contain two items.", True,
+                     ValueError)
 
-    input_path = os.path.abspath(input_path)
+    file_path = os.path.abspath(file_path)
     for item in list_files:
         path_compare = os.path.abspath(str(item[0]))
         name_compare = str(item[1])
-        if input_path == path_compare:
+        if file_path == path_compare:
             __ex("The %s and the %s file path must not be identical." %
-                 (name, name_compare), False)
-        if os.path.exists(input_path) and os.path.exists(path_compare):
-            if filecmp.cmp(input_path, path_compare, 0):
+                 (name, name_compare), False, ValueError)
+        if os.path.exists(file_path) and os.path.exists(path_compare):
+            if filecmp.cmp(file_path, path_compare, 0):
                 __ex("The %s and %s file content must not be identical." %
-                     (name, name_compare), False)
+                     (name, name_compare), False, ValueError)
 
 
 def compstr(input_string, name="", list_strings=None):
@@ -54,9 +57,10 @@ def compstr(input_string, name="", list_strings=None):
     """
     __string(input_string, name, False)
     if not list_strings:
-        __ex("No %s strings to compare with." % name, True)
+        __ex("No %s strings to compare with." % name, True, ValueError)
     if input_string not in list_strings:
-        __ex("The %s '%s' does not exist." % (name, input_string), False)
+        __ex("The %s '%s' does not exist." % (name, input_string), False,
+             FileNotFoundError)
 
 
 def get_version():
@@ -79,26 +83,27 @@ def intrange(value, name="", value_min=None, value_max=None, zero=False):
         intvalue(value_max, name, True, True, True)
     if not zero:
         if value == 0:
-            __ex("The %s value must not be zero." % name, False)
+            __ex("The %s value must not be zero." % name, False, ValueError)
     if (value_min is not None) and (value_max is not None):
         if value_min > value_max:
             __ex("The maximal %s value must be greater than the minimal "
-                 "value." % name, False)
+                 "value." % name, False, ValueError)
         if (value_min == value_max) and (value != value_min):
             __ex("The %s value can only be %s (depending on further range "
-                 "further range arguments)." % (name, value_min), False)
+                 "further range arguments)." % (name, value_min), False,
+                 ValueError)
         if (value < value_min) or (value > value_max):
             __ex("The %s value must be between %s and %s (depending on "
                  "further range arguments)." % (name, value_min, value_max),
-                 False)
+                 False, ValueError)
     elif value_min is not None:
         if value < value_min:
             __ex("The %s value must not be less than %s." % (name, value_min),
-                 False)
+                 False, ValueError)
     elif value_max is not None:
         if value > value_max:
             __ex("The %s value must not be greater than %s." %
-                 (name, value_max), False)
+                 (name, value_max), False, ValueError)
 
 
 def intvalue(value, name="", positive=True, zero=False, negative=False):
@@ -108,37 +113,67 @@ def intvalue(value, name="", positive=True, zero=False, negative=False):
     value = __integer(value, "%s value" % name, False)
     if not positive:
         if value > 0:
-            __ex("The %s value must not be positive." % name, False)
+            __ex("The %s value must not be positive." % name, False,
+                 ValueError)
     if not zero:
         if value == 0:
-            __ex("The %s value must not be zero." % name, False)
+            __ex("The %s value must not be zero." % name, False, ValueError)
     if not negative:
         if value < 0:
-            __ex("The %s value must not be negative." % name, False)
+            __ex("The %s value must not be negative." % name, False,
+                 ValueError)
 
 
-def path(input_path, name="", is_file=False, exists=False):
+def path(pathname, name="", is_file=False, exists=False):
     """
         Validate a path of a file or directory.
     """
-    string(input_path, "%s path" % name, False, None)
-    input_path = os.path.abspath(input_path)
+    string(pathname, "%s path" % name, False, None)
+    pathname = os.path.abspath(pathname)
 
     if is_file:
         path_type = "file"
     else:
         path_type = "directory"
     if exists:
-        if not os.path.exists(input_path):
-            __ex("The given %s %s does not exist." % (name, path_type), False)
-        if (is_file and not os.path.isfile(input_path)) or \
-           (not is_file and not os.path.isdir(input_path)):
+        if not os.path.exists(pathname):
+            __ex("The given %s %s does not exist." % (name, path_type), False,
+                 FileNotFoundError)
+        if (is_file and not os.path.isfile(pathname)) or \
+           (not is_file and not os.path.isdir(pathname)):
             __ex("The given %s %s path is not a %s." % (name, path_type,
                                                         path_type), False)
     else:
-        if os.path.exists(input_path):
+        if os.path.exists(pathname):
             __ex("The given %s %s path already exists." % (name, path_type),
-                 False)
+                 False, FileExistsError)
+
+
+def param_type_list(param_list=[], strict=True):
+    """
+        Validate parameter types using a list.
+    """
+    for param in param_list:
+        p_value = [param][0][0]  # value
+        p_desc = [param][0][1]   # description
+        p_type = [param][0][2]   # expected value type
+
+        if not strict:
+            if type(p_value) == int and p_type == float:
+                # In case a float is expected and an integer is given, convert
+                # the integer to float, e.g. '2' to '2.0'
+                p_value = float(p_value)
+
+        if p_type is None:
+            __ex("Parameter '%s' must not be 'NoneType'." % p_desc, TypeError)
+
+        if not isinstance(p_value, p_type):
+            # Quick-and-dirty solution without using regular expressions.
+            # Works as expected at least.
+            p_type = repr(p_type).split()[1].rstrip(">")
+
+            __ex("Parameter '%s' is not type of %s." % (p_desc, p_type),
+                 TypeError)
 
 
 def string(input_string, name="", wildcards=False, invalid_chars=None):
@@ -150,7 +185,8 @@ def string(input_string, name="", wildcards=False, invalid_chars=None):
         invalid_chars = ""
     if not wildcards:
         if ("*" in input_string) or ("?" in input_string):
-            __ex("The %s must not contain wildcards." % name, False)
+            __ex("The %s must not contain wildcards." % name, False,
+                 ValueError)
     if invalid_chars:
         for char in invalid_chars:
             if char in input_string:
@@ -161,10 +197,11 @@ def string(input_string, name="", wildcards=False, invalid_chars=None):
                     quotes = "\""
 
                 __ex("The %s contains at least one invalid character "
-                     "(%s%s%s)." % (name, quotes, char, quotes), False)
+                     "(%s%s%s)." % (name, quotes, char, quotes), False,
+                     ValueError)
 
 
-def __ex(exception_string, internal=False):
+def __ex(exception_string, internal=False, exception_type=TypeError):
     """
         Internal method to raise an exception.
     """
@@ -173,7 +210,7 @@ def __ex(exception_string, internal=False):
         ex = ex.replace((" " * 2), " ")
     if internal:
         ex = "PaVal: " + ex
-    raise Exception(ex)
+    raise exception_type(ex)
 
 
 def __integer(value, name="", internal=False):
@@ -181,13 +218,13 @@ def __integer(value, name="", internal=False):
         Internal method for basic integer validation.
     """
     if value is None:
-        __ex("The %s is missing." % name, internal)
+        __ex("The %s is missing." % name, internal, ValueError)
     if value == "":
-        __ex("The %s must not be empty." % name, internal)
+        __ex("The %s must not be empty." % name, internal, ValueError)
     try:
         value = int(value)
     except ValueError:
-        __ex("The %s must be an integer." % name, internal)
+        __ex("The %s must be an integer." % name, internal, TypeError)
     return int(value)
 
 
@@ -196,8 +233,6 @@ def __string(input_string, name="", internal=False):
         Internal method for basic string validation.
     """
     if input_string is None:
-        __ex("The %s is missing." % name, internal)
+        __ex("The %s is missing." % name, internal, ValueError)
     if input_string == "":
-        __ex("The %s must not be empty." % name, internal)
-
-# EOF
+        __ex("The %s must not be empty." % name, internal, ValueError)
